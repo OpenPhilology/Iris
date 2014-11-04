9881fc71dc06182ca297c599476a40657c4b7f47
greekOCR <openphilology@informatik.uni-leipzig.de>
2014-11-04 17:46:58 +0100

    Created lex.py with tests for storing dictionary tools.

--- iris/lex.py
+++ iris/lex.py
@@ -3,15 +3,59 @@
 # such as extracting words from texts, normalizing encodings, building
 # symmetric deletion dictionaries, etc.
 
+import os
 import codecs
+import glob
 import algorithms as alg
 
 def cleanlines(path, encoding=u'utf-8', normalization=u'NFD'):
     """
     Read in lines from a file and return them as a sanitized list.
+    Non-unique linse will be repeated.
     """
+    words = []
     with codecs.open(path, u'r', encoding=encoding) as lines:
-        words = []
         for line in lines:
             words.append(alg.sanitize(line, normalization=normalization))
-        return words
+    return words
+
+def cleanwords(path, encoding=u'utf-8', normalization=u'NFD'):
+    """
+    Read in every word from a files as separated by lines and spaces.
+    Non-unique words will be repeated as they are read in.
+    """
+    words = []
+    with codecs.open(path, u'r', encoding=encoding) as lines:
+        for line in lines:
+        	for seg in line.split(u' '):
+        		clean = alg.sanitize(seg, normalization=normalization)
+        		if clean != u'':
+        			words.append(clean)
+    return words
+
+def cleanuniquewords(path, encoding=u'utf-8', normalization=u'NFD'):
+    """
+    Read in lines from a file as separated by lines and spaces,
+    convert them to the specified normalization, andreturn a list
+    of all unique words.
+    """
+    return set(cleanwords(path, encoding=encoding, normalization=normalization))
+
+
+def words_from_files(dirpath, encoding=u'utf-8', normalization=u'NFD'):
+    """
+    Create a dictionary from a directory of text files.
+	All file in the given directory will be parsed.
+    """
+    words = []
+    for filename in filter(os.path.isfile, glob.glob(dirpath + '/*')):
+        words += cleanwords(filename, encoding=encoding, normalization=normalization)
+    return words
+
+
+def unique_words_from_files(dirpath, encoding=u'utf-8', normalization=u'NFD'):
+	"""
+	Create a set of unique words from a directory of text files.
+	All file in the given directory will be parsed.
+	"""
+	return set(words_from_files(dirpath, encoding=encoding, normalization=normalization))
--- tests/test_lex.py
+++ tests/test_lex.py
@@ -32,6 +32,80 @@ class DictTests(unittest.TestCase):
         self.assertEqual(words[2], u'αχιλλεύς')
         self.assertEqual(words[3], u'αχιλλεύς')
 
+    def test_cleanwords(self):
+        """
+        Test the cleanwords function.
+        """
+        self.temp.write(u'adding a line with multiple words\n'.encode(u'utf-8'))
+        self.temp.write(u'another with Greek αχιλλεύς\n'.encode(u'utf-8'))
+        self.temp.write(u'and some NFC αχιλλεύς\n'.encode(u'utf-8'))
+        self.temp.seek(0, 0)
+        expected = [u'adding', u'a', u'line', u'with', u'multiple', u'words',
+                    u'another', u'with',  u'Greek', u'αχιλλεύς', u'and',
+                    u'some', u'NFC', u'αχιλλεύς']
+        words = lex.cleanwords(self.path)
+        self.assertEqual(words, expected)
+
+    def test_cleanuniquewords(self):
+        """
+        Test the cleanuniquewords function.
+        """
+        self.temp1 = tempfile.NamedTemporaryFile()
+        self.temp1.write(u'adding a line with multiple words\n'.encode(u'utf-8'))
+        self.temp1.write(u'another with Greek αχιλλεύς\n'.encode(u'utf-8'))
+        self.temp1.write(u'and some NFC αχιλλεύς\n'.encode(u'utf-8'))
+        self.temp1.seek(0, 0)
+        expected = set([u'adding', u'a', u'line', u'with', u'multiple', u'words',
+                    u'another', u'with',  u'Greek', u'αχιλλεύς', u'and',
+                    u'some', u'NFC', u'αχιλλεύς'])
+        self.assertEqual(lex.cleanuniquewords(self.temp1.name), expected)
+
+    def test_words_from_files(self):
+        """
+        Test the words_from_files function.
+        """
+        dirpath = tempfile.mkdtemp()
+        self.temp1 = tempfile.NamedTemporaryFile(dir=dirpath)
+        self.temp2 = tempfile.NamedTemporaryFile(dir=dirpath)
+        self.temp3 = tempfile.NamedTemporaryFile(dir=dirpath)
+        self.dirtofilter = tempfile.mkdtemp(dir=dirpath) #To be filtered out
+        self.temp1.write(u'a')
+        self.temp2.write(u'b')
+        self.temp3.write(u'c')
+        self.temp1.seek(0, 0)
+        self.temp2.seek(0, 0)
+        self.temp3.seek(0, 0)
+        words = lex.words_from_files(dirpath)
+        self.assertEqual(3, len(words))
+        self.assertTrue(u'a' in words)
+        self.assertTrue(u'b' in words)
+        self.assertTrue(u'c' in words)
+
+    def test_unique_words_from_files(self):
+        """
+        Test the words_from_files_function.
+        """
+        dirpath = tempfile.mkdtemp()
+        self.temp1 = tempfile.NamedTemporaryFile(dir=dirpath)
+        self.temp2 = tempfile.NamedTemporaryFile(dir=dirpath)
+        self.temp3 = tempfile.NamedTemporaryFile(dir=dirpath)
+        self.temp4 = tempfile.NamedTemporaryFile(dir=dirpath)
+        self.dirtofilter = tempfile.mkdtemp(dir=dirpath) #To be filtered out
+        self.temp1.write(u'a')
+        self.temp2.write(u'b b')
+        self.temp3.write(u'c a')
+        self.temp4.write(u'a b c \nd')
+        self.temp1.seek(0, 0)
+        self.temp2.seek(0, 0)
+        self.temp3.seek(0, 0)
+        self.temp4.seek(0, 0)
+        words = lex.unique_words_from_files(dirpath)
+        self.assertEqual(4, len(words))
+        self.assertTrue(u'a' in words)
+        self.assertTrue(u'b' in words)
+        self.assertTrue(u'c' in words)        
+        self.assertTrue(u'd' in words)        
+
 
 
 if __name__ == '__main__':
